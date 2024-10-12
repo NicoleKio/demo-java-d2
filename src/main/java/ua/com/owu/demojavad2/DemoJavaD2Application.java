960e9d118ca593a55c11e01fbc974bdd42a0d4e8
NicoleKio <nastiamay.1654@gmail.com>
2024-10-12 20:04:54 +0300

    Initial commit

---
+++ src/main/java/ua/com/owu/demojavad2/DemoJavaD2Application.java
@@ -0,0 +1,13 @@
+package ua.com.owu.demojavad2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJavaD2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJavaD2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ua/com/owu/demojavad2/DemoJavaD2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.owu.demojavad2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJavaD2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
