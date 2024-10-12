2449a3bcf2ecce874e80d4684784e091a76f0e2a
NicoleKio <nastiamay.1654@gmail.com>
2024-10-12 20:05:14 +0300

    master-main-commit

--- src/test/java/ua/com/owu/demojavad2/DemoJavaD2ApplicationTests.java
+++ src/test/java/ua/com/owu/demojavad2/DemoJavaD2ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemoJavaD2ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
