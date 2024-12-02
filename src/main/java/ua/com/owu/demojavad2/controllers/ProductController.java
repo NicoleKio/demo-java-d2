5c42b69c94c2b679ea4f5525328c445c7547aa1e
NicoleKio <nastiamay.1654@gmail.com>
2024-12-02 21:42:15 +0200

    the last try // fail again -> create a new p

--- src/main/java/ua/com/owu/demojavad2/controllers/ProductController.java
+++ src/main/java/ua/com/owu/demojavad2/controllers/ProductController.java
@@ -1,48 +1,106 @@
 package ua.com.owu.demojavad2.controllers;
 
-import com.mongodb.client.model.vault.RewrapManyDataKeyOptions;
-import lombok.AllArgsConstructor;
-import org.bson.types.ObjectId;
-import org.springframework.beans.factory.xml.ResourceEntityResolver;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ua.com.owu.demojavad2.dto.ProductDTO;
 import ua.com.owu.demojavad2.dto.ReviewDTO;
 import ua.com.owu.demojavad2.entities.Product;
-import ua.com.owu.demojavad2.entities.Review;
 import ua.com.owu.demojavad2.repository.ProductRepository;
 import ua.com.owu.demojavad2.service.ProductService;
 
 import java.util.List;
-import java.util.ResourceBundle;
+
 
 @RestController
-@AllArgsConstructor
-@RequestMapping("/products")
+@RequiredArgsConstructor
 public class ProductController {
 
     private final ProductRepository productRepository;
     private final ProductService productService;
 
-    @GetMapping()
-    public ResponseEntity <List<ProductDTO>> getAllProducts() {
-        return ResponseEntity.ok(productService.getProducts());
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable Long id) {
+        return ResponseEntity.of(this.productRepository.findById(id));
     }
 
-    @PostMapping()
-    public ResponseEntity<ProductDTO> addProduct(@RequestBody ProductDTO productDTO) {
-        return ResponseEntity.ok(productService.addProduct(productDTO));
+//    @GetMapping("/products")
+//    public ResponseEntity<List<Product>> getProducts(
+//            @RequestParam(name = "minPrice", required = false) Double minPrice,
+//            @RequestParam(name = "maxPrice", required = false) Double maxPrice
+//    ) {
+//
+//        if (minPrice != null && maxPrice != null) {
+//            return ResponseEntity.ok(this.productRepository.findAllByPriceBetween(minPrice, maxPrice));
+//        } else if (minPrice != null) {
+//            return ResponseEntity.ok(this.productRepository.findAllByPriceGreaterThan(minPrice));
+//        } else if (maxPrice != null) {
+//            return ResponseEntity.ok(this.productRepository.findAllByPriceLessThan(maxPrice));
+//        } else {
+//            return ResponseEntity.ok(this.productRepository.findAll());
+//        }
+//    }
+
+    @PostMapping("/products")
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        return ResponseEntity.ok(this.productRepository.save(product));
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ProductDTO> getProduct(@PathVariable Long id) {
-        return ResponseEntity.ok(productService.getById(id));
+    @Transactional
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable(name = "id") Long productId, @RequestBody Product product) {
+        return ResponseEntity.of(
+                this.productRepository
+                        .findById(productId)
+                        .map(oldProduct -> {
+                            oldProduct.setName(product.getName());
+                            oldProduct.setPrice(product.getPrice());
+                            oldProduct.setAvailability(product.getAvailability());
+//                            return productRepository.save(oldProduct); // save is not required as there is @Transactional
+                            return oldProduct;
+                        })
+        );
     }
 
-    @PostMapping("/{id}/reviews")
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable(name = "id") Long productId) {
+        this.productRepository.deleteById(productId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/products/{id}/reviews")
     public ResponseEntity<ReviewDTO> createReview(@PathVariable(name = "id") Long productId, @RequestBody ReviewDTO reviewDTO) {
         return ResponseEntity.ok(this.productService.createReview(productId, reviewDTO));
     }
+
+    @GetMapping("/products/{id}/reviews")
+    public ResponseEntity<List<ReviewDTO>> getReviews(@PathVariable Long id) {
+        return ResponseEntity.ok(this.productService.findAllReviewsForProduct(id));
+    }
+
+    //----------------- my --------------------
+
+//    @GetMapping("/products")
+//    public ResponseEntity <List<ProductDTO>> getAllProducts() {
+//        return ResponseEntity.ok(productService.getProducts());
+//    }
+//
+//    @PostMapping("/products")
+//    public ResponseEntity<ProductDTO> addProduct(@RequestBody ProductDTO productDTO) {
+//        return ResponseEntity.ok(productService.addProduct(productDTO));
+//    }
+
+    //-----12--------------------------------------
+//    @GetMapping("/{id}")
+//    public ResponseEntity<ProductDTO> getProduct(@PathVariable Long id) {
+//        return ResponseEntity.ok(productService.getById(id));
+//    }
+//
+//    @PostMapping("/{id}/reviews")
+//    public ResponseEntity<ReviewDTO> createReview(@PathVariable(name = "id") Long productId, @RequestBody ReviewDTO reviewDTO) {
+//        return ResponseEntity.ok(this.productService.createReview(productId, reviewDTO));
+//    }
 
 
 }
